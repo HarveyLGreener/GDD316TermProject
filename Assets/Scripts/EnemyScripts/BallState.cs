436334f508fffdd56c9b0ade2c86c8ba21cdbd5a
HarveyLGreener <therebelrangerlemonpop@gmail.com>
2024-05-09 00:26:17 -0400

    HE SHOOTS... AND SCORES
    Dear fucking god why was that such a PAIN IN MY ASS?!?!?!?!?

--- Assets/Scripts/EnemyScripts/BallState.cs
+++ Assets/Scripts/EnemyScripts/BallState.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class BallState : AgentBaseState
 {
+    private float shotWaitTimer = 0f;
+    private bool timerRestart = true;
     public override void EnterState(StateController agent)
     {
         agent.meshRenderer.material.color = Color.red;
@@ -15,11 +17,29 @@ public class BallState : AgentBaseState
     }
     public override void Update(StateController agent)
     {
-        if (Mathf.Abs((agent.gameObject.transform.position - agent.shootPos.position).magnitude) < 5f)
+        if (shotWaitTimer > 0f)
         {
-            Debug.Log("taking the shot");
+            shotWaitTimer -= Time.deltaTime;
         }
-        else if (agent.gameObject.transform.GetChildCount() > 0)
+        if (Mathf.Abs((agent.gameObject.transform.position - agent.shootPos.position).magnitude) < 5f && agent.gameObject.transform.childCount > 0)
+        {
+            Debug.Log("Added Force");
+            if (timerRestart)
+            {
+                timerRestart = false;
+                agent.ball.GetComponent<Rigidbody>().isKinematic = false;
+                agent.ball.GetComponent<Rigidbody>().AddForce(new Vector3(-0.5f, 15f, -10f), ForceMode.Impulse);
+                shotWaitTimer = 3f;
+
+            }
+            if (shotWaitTimer <= 0f)
+            {
+                agent.transform.DetachChildren();
+                timerRestart = true;
+            }
+            //agent.TransitionToState(agent.chaseState);
+        }
+        else if (agent.gameObject.transform.childCount > 0)
         {
             agent.agent.SetDestination(agent.shootPos.position);
         }
@@ -29,4 +49,5 @@ public class BallState : AgentBaseState
         }
 
     }
+    
 }
